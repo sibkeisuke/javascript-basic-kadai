0f43b3181f09910a1fdceeb145b3bd7d1e67c4eb
sibkeisuke <167329652+sibkeisuke@users.noreply.github.com>
2024-05-17 11:35:00 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
 const displayTodaysDate = () => {
   const today = new Date();
   const year = today.getFullYear();
-  const month = today.getMonth();
+  const month = today.getMonth() + 1;
   const date = today.getDate();
 console.log(year + '年' + month + '月' + date + '日');
 }
